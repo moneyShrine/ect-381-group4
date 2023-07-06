1a648b87a2b1b2e6d4d610ce7c85a3dc41153eb7
PETER ONYAM <peteronyam@gmail.com>
2023-07-06 22:33:42 +0100

    Update Formula2.h
    Update header file

--- group4/Formula2.h
+++ group4/Formula2.h
@@ -1,5 +1,5 @@
 #define FORMULA2_H
-
+//This header file is created by UKAH GAIUS RAYMOND: ECT : 19/097344153
 double calculateAcceleration(double value1, double value2, double value3) {
     return (value2 - value1) / value3;
 };
