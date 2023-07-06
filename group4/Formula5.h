7c6ea9357a4d19bc7f0eafc09d33c9741170e693
PETER ONYAM <peteronyam@gmail.com>
2023-07-06 22:35:45 +0100

    Update Formula5.h
    Update header file

--- group4/Formula5.h
+++ group4/Formula5.h
@@ -1,5 +1,5 @@
 #define FORMULA5_H
-
+//This header file is created by UKAM SAVIOUR AJAH: ECT: 19/097344154
 double calculateEnergy(double value1, double value2) {
     return 0.5 * value1 * value2 * value2;
 };
