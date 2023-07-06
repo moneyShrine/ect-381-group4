05bf2491a63a5dd0544c3f7236b562e4656703d8
PETER ONYAM <peteronyam@gmail.com>
2023-07-06 22:35:05 +0100

    Update Formula4.h
    Update header file

--- group4/Formula4.h
+++ group4/Formula4.h
@@ -1,5 +1,5 @@
 #define FORMULA4_H
-
+//This header file is created by INIKO EMMANUELLA EKENG: ECT : 19/097344078
 double calculateForce(double value1, double value2) {
     return value1 * value2;
 };
