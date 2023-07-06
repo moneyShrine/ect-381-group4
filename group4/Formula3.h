6062ea93cdcfafe7f8dd6aeb0d85d0e9b9450911
PETER ONYAM <peteronyam@gmail.com>
2023-07-06 22:34:23 +0100

    Update Formula3.h
    Update header file

--- group4/Formula3.h
+++ group4/Formula3.h
@@ -1,5 +1,5 @@
 #define FORMULA3_H
-
+//This header file is created by OGRI EMMANUEL ADANU: ECT : 19/097344115
 double calculateDistance(double value1, double value2, double value3) {
     return (value1 * value2) + (0.5 * value3 * value3);
 };
